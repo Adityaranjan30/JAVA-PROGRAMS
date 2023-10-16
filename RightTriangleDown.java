4ab33826614da3a0f84d5d64847beb49d7a66233
Adityaranjan30 <131745092+Adityaranjan30@users.noreply.github.com>
2023-10-16 11:22:06 +0530

    Add files via upload

---
+++ RightTriangleDown.java
@@ -0,0 +1,13 @@
+public class RightTriangleDown {
+    public static void main(String[] args){
+        for(int i=0;i<6;i++)
+        {
+            for(int j=i;j<6;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
